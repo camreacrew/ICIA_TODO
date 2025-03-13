1cc1e65a55da1c04be285b8aa551df3bd7e59459
cameracrew <kimhyojae2053@gmail.com>
2025-03-13 16:56:06 +0900

    완성본

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from contact_view import contact_app
 from finance_view import finance_app
 from supply_view import supply_app
+from todo_view import todo_app
 
 
 
@@ -10,10 +11,8 @@ app = Flask(__name__)
 app.register_blueprint(contact_app)
 app.register_blueprint(finance_app)
 app.register_blueprint(supply_app)
+app.register_blueprint(todo_app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-app.run(debug=True)
--- todo_view.py
+++ todo_view.py
@@ -1,16 +1,16 @@
-from flask import Flask, redirect, render_template, request
+from flask import Blueprint, redirect, render_template, request
 import datetime as dt
 
-app = Flask(__name__)
+todo_app = Blueprint('todo_app', __name__, url_prefix='/todo')
 
 todos = []
 tno = 1
 
-@app.route("/todo")
+@todo_app.route("/list")
 def todo():
-  return render_template("/todo/list.html", todos=todos)
+  return render_template("todo/list.html", todos=todos)
 
-@app.route("/write", methods=['post'])
+@todo_app.route("/write", methods=['post'])
 def write():
   global tno
   title = request.form.get('title')
@@ -18,31 +18,20 @@ def write():
   new_todo = {'tno':tno, 'title':title, 'date':date, 'finish':False}
   todos.append(new_todo)
   tno+=1
-  return redirect("/todo")
+  return redirect("/todo/list")
 
-@app.route("/finish", methods=['post'])
+@todo_app.route("/finish", methods=['post'])
 def finish():
-  try:
     tno = int(request.form.get('tno'))
     for todo in todos:
       if todo['tno'] == tno:
         todo['finish'] = True
-  except (ValueError, TypeError):
-    print("유효하지 않은 tno값이 들어왔습니다.")
-  return redirect("/todo")
+    return redirect("/todo/list")
 
-@app.route("/delete", methods=['post'])
+@todo_app.route("/delete", methods=['post'])
 def delete():
-  try:
     tno = int(request.form.get('tno'))
     for todo in todos:
       if todo['tno'] == tno:
         todos.remove(todo)
-  except (ValueError, TypeError):
-    print("유효하지 않은 tno 값이 들어왔습니다.")  
-  return redirect("/todo")
-
-
-
-
-app.run(debug=True)
+    return redirect("/todo/list")
